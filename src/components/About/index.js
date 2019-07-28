47c744933f0269292cc6a95d1f9c957c000a5d4e
Floyd Orr <floydcanhelp@gmail.com>
2019-07-28 12:15:46 -0500

    add reactstrap

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import {Button} from 'reactstrap';
 const About =()=> {
   return (
+    <>
      <p>About Page</p>
+     <Button color="danger">Reactstrap Button Test</Button>
+    </>
   )
 }
 
